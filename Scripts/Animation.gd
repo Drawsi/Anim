d3f28e04a46d767423017f3460da1f2421758950
Drawsi <drawsiyt@gmail.com>
2023-04-05 14:58:42 +0300

    Version_1.3.5
    Now the frame nodes are in order no matter the way they are added.

--- Scripts/Animation.gd
+++ Scripts/Animation.gd
@@ -14,7 +14,11 @@ var find
 var verify
 var le_onion = false
 
+func _ready():
+	_sort_frames()
+
 func _process(_delta):
+	_onion()
 	#Initializing variables and values needed
 	$TimelineSettings/Current_Fps/SpinBox.value = Global.current_frame
 	Global.set_fps = $TimelineSettings/Fps_Counter/Run_FPS.value
@@ -34,49 +38,30 @@ func _process(_delta):
 		find.queue_free()
 	
 func _sort_frames():
-	var all = subview.get_children()
-	#var found
-	#for this in subview.get_children():
-	#	var current = int(str(this.name))
-	#	if int(str(this.name))>subview.get_child_count():
-	#		while !found and current>0:
-	#			found = get_node_or_null(str(current))
-	#			current = current-1
-	#		subview.move_child(this,subview.get_child_count()-current)
-	#	else:
-	#		subview.move_child(this,int(str(this.name)))
-	for i in range(all.size()-1, -1, -1):
-		for j in range(1,i+1,1):
-			if all[j-1].name > all[j].name:
-				var aux = all[j-1]
-				#all[j-1] = all[j]
-				#all[j] = aux
-				move_child(all[j-1],all[j].get_index())
-				move_child(all[j],aux.get_index())
-	#for child in all:
-	#	subview.add_child(child)
-	print(all)
-	#subview.print_tree_pretty()
+	#Code provided by Joel Gomes da Silva
+	"""https://godotengine.org/qa/150583/how-can-i-sort-the-children-of-a-node?state=comment-150588&show=150588#c150588"""
+	var sorted_nodes := subview.get_children()
+	sorted_nodes.sort_custom(func(a: Node, b: Node): return a.name.naturalnocasecmp_to(b.name) < 0)
 
+	for node in subview.get_children():
+		subview.remove_child(node)
+	for node in sorted_nodes:
+		subview.add_child(node)
+	subview.print_tree_pretty()
+	
 func _onion():
 	#	Onion-Skin
-	if subview.get_child_count()>1 and verify:
-		#all = bubble_sort(subview.get_children())
-		var _found = null
-		var _n = 2
-		subview.get_children()
-		subview.get_children().clear()
-		#subview.get_children().append_array(bubble_sort(subview.get_children()))
-		#print(subview.get_children())
-		#while !found:
-		#	#if all[Global.current_frame-n]!=get_node_or_null():
-		#	if get_node_or_null(str(Global.current_frame-n)):
-		#		found = all[Global.current_frame-n]
-		#		n = 2
-		#	else:
-		#		n = n+1
-		#found.visible = true
-		#found.set_modulate(Color('626fff'))#ffffff
+	if verify:
+		var prev
+		if Global.current_frame>subview.get_child_count():
+			prev = subview.get_child(Global.current_frame-2)
+			prev.visible = true
+			prev.set_modulate(Color('626fff'))#ffffff
+		else :
+			prev = subview.get_child(subview.get_child_count()-1)
+			prev.visible = true
+			prev.set_modulate(Color('626fff'))#ffffff
+		
 
 func _inviz():
 	for i in subview.get_children():
